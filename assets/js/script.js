5441cd90c6e2578b72f5d7d53e21f04fe19cffc7
Tamus Tahir <tamusbintahir@gmail.com>
2024-01-17 23:53:23 +0800

    pertemuan 28

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+new PureCounter();
+
+AOS.init({
+    duration: 1000,
+});
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
